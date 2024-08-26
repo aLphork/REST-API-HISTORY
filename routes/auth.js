67d009ad988d789e673b94464985dfcd9c5755eb
Zahid <zahiddemirbilek7@gmail.com>
2024-08-26 08:57:11 +0300

    restApiHistV1.3

--- routes/auth.js
+++ routes/auth.js
@@ -1,49 +1,58 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/Users');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-  const { name, surname, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+router.post("/register", async (req, res) => {
+	const { name, surname, password: rawPassword } = req.body;
+	const password = await bcrypt.hash(rawPassword, 10);
 
-  try {
-    const user = await User.create({ name, surname, password: hashedPassword });
-      res.status(201).json({ message: 'Kayit başarili.' });
+	try {
+		const foundUser = await User.findOne({ where: { name } });
+		console.log(req.body, foundUser);
 
-    const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
-      res.json({ token: token });
-  } catch (error) {
-      res.status(500).json({ error: error.message });
-  }
+		if (!foundUser || foundUser.name === null) {
+			const user = await User.create({ name, surname, password });
+
+			return res.json({
+				token: jwt.sign({ id: user.id }, "your_jwt_secret", {
+					expiresIn: "1h",
+				}),
+				message: "Kayit başarili.",
+			});
+		}
+
+		if (foundUser.name === name) {
+			return res.status(400).json({ message: "Kullanici mevcut." });
+		}
+	} catch (error) {
+		return res.status(500).json(error);
+	}
 });
 
-router.post('/login', async (req, res) => {
-  const { name, password } = req.body;
+router.post("/login", async (req, res) => {
+	const { name, password } = req.body;
 
-  const user = await User.findOne({ where: { name: name} });
-  console.log(req.body, user)
-  if (!user) return res.status(400).json({ message: 'Kullanici bulunamadi.' });
+	const user = await User.findOne({ where: { name: name } });
+	console.log(req.body, user);
+	if (!user) return res.status(400).json({ message: "Kullanici bulunamadi." });
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-        return res.status(400).json({ message: 'Geçersiz şifre.' });
-  } else {
-    const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
-    res.json({ token: token });
-        return(res.json({ message: 'Giriş başarili.' }))
-  }
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) {
+		return res.status(400).json({ message: "Geçersiz şifre." });
+	}
+
+	const token = jwt.sign({ id: user.id }, "your_jwt_secret", {
+		expiresIn: "1h",
+	});
+
+	return res.json({ token: token, message: "Giriş başarili." });
 });
 
 module.exports = router;
 
-
-
-
-
-//register da kullanıcı muvecut uyarısı.
-//token registerde 
-//hataalı girişler için hata mesajları 
-//update de mevcut birinin isimlerini yazamasın
+//token registerde                      (okey)
+//register da kullanıcı muvcut uyarısı. (okey)
+//hatalı girişler için hata mesajları   (okey)
--- routes/users.js
+++ routes/users.js
@@ -1,72 +1,75 @@
-
-const express = require('express');
-const authenticateToken = require('../middleware/auth');
-const User = require('../models/Users');
-const UserHistory = require('../models/UsersHistory');
+const express = require("express");
+const authenticateToken = require("../middleware/auth");
+const User = require("../models/Users");
+const UserHistory = require("../models/UsersHistory");
 
 const router = express.Router();
 
+router.get("/getAll", authenticateToken, async (req, res) => {
+	try {
+		const users = await User.findAll();
 
-router.get('/getAll', authenticateToken, async (req, res) => {
-  try{
-    const users = await User.findAll();
+		if (!users || users.length === 0)
+			return res.status(500).json({ message: "Kullanicilar bulunamadı." });
+		console.log(req.query, users);
 
-    if( !users || users.length === 0 ) return res.status(500).json({ message: 'Kullanicilar bulunamadı.'});
-    console.log(req.query, users);
-    
-    return res.json({ users });
-    
-  } catch(error){
-    console.log(error);
-    return res.status(500).json({ message: 'Sunucu hatasi.'})
-  }
+		return res.json({ users });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Sunucu hatasi." });
+	}
 });
-router.put('/update/:id', authenticateToken, async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+router.put("/update/:id", authenticateToken, async (req, res) => {
+	const user = await User.findByPk(req.params.id);
 
-  if (!user) return res.status(404).json({ message: 'Kullanici bulunamadi.' });
+	if (!user) return res.status(404).json({ message: "Kullanici bulunamadi." });
 
-  // Kullanıcının güncel durumunu history tablosuna kaydet
-  try {
-    await UserHistory.create({
-      userId: user.id,
-      changeDate: new Date(),
-      count: user.changeCount + 1,
-      previousName: user.name,
-      previousSurname: user.surname,
-    });
-
+	// Kullanıcının güncel durumunu history tablosuna kaydet
+	try {
     const { name, surname } = req.body;
-    await user.update({ name, surname, changeCount: user.changeCount + 1 });
 
-    // 3 değişiklik hakkını dolduran kullanıcıyı sil
-    if (user.changeCount >= 3) {
-      await user.destroy();
-      return res.status(403).json({ message: '3 değişiklik oldu kullanici silindi.' });
-    }
+    if (user.name === name && user.surname === surname)
+      return res.json({ message: "Kullanici mevcut." });
 
-    res.json({ message: 'Kullanici güncellendi.', user });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+		await UserHistory.create({
+			userId: user.id,
+			changeDate: new Date(),
+			count: user.changeCount + 1,
+			previousName: user.name,
+			previousSurname: user.surname,
+		});
+
+		await user.update({ name, surname, changeCount: user.changeCount + 1 });
+
+		// 3 değişiklik hakkını dolduran kullanıcıyı sil
+		if (user.changeCount >= 3) {
+			await user.destroy();
+			return res
+				.status(403)
+				.json({ message: "3 değişiklik oldu kullanici silindi." });
+		}
+
+		res.json({ message: "Kullanici güncellendi.", user });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 });
 
+router.delete("/delete/:id", authenticateToken, async (req, res) => {
+	try {
+		const user = await User.findByPk(req.params.id);
+		console.log(user);
 
+		if (!user) return res.json({ message: "Kullanici bulunamadi." });
 
-router.delete('/delete/:id', authenticateToken, async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id);
-    console.log("salamlar");
-      console.log(user);
-
-    if ( !user ) return res.json({ message: 'Kullanici bulunamadi.' });
-    
-    await user.destroy();
-    res.json({ message: 'Kullanici silindi.'})
-
-  } catch (error) {
-    res.status(500).json({ message: 'Sunucu hatasi.'})
-  }
+		await user.destroy();
+		res.json({ message: "Kullanici silindi." });
+	} catch (error) {
+		res.status(500).json({ message: "Sunucu hatasi." });
+	}
 });
 
 module.exports = router;
+
+//delete çalıştır                                                              (okey)
+//update de mevcut birinin isimlerini yazamasın(id, name ve surname kıyasla)   (okey)
